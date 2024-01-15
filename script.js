f58672f78beb7c803c3e0637875ac140fda15650
TheBreakfastBun <138614756+TheBreakfastBun@users.noreply.github.com>
2024-01-15 19:14:26 +0200

    Change Grid with Prompt completed

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const body = document.querySelector('body');
+
+const btnGrid = document.createElement('button');
+btnGrid.textContent = "Change Grid Layout";
+body.appendChild(btnGrid);
+
 const container = document.createElement("div");
 container.style = "display: flex; height: 100vh; width: 100%;"
 body.appendChild(container);
@@ -16,3 +21,31 @@ for (let x = 0; x < 16; ++x){
         })
     }
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+btnGrid.addEventListener('click', () => {
+    removeAllChildNodes(container);
+
+    let gridSize = prompt("How many squares per side do you want?");
+
+        for (let x = 0; x < gridSize; ++x){
+            const div = document.createElement('div');
+            div.style = "border: 1px solid white; flex: auto; display: flex; flex-direction: column;";
+            container.appendChild(div);
+                for (let y = 0; y < gridSize; ++y){
+                const yDiv = document.createElement('div');
+                yDiv.style = "border: 1px solid white; flex: auto;";
+                div.appendChild(yDiv);
+                yDiv.addEventListener('mouseover', () => {
+                    yDiv.style = "border: 1px solid white; flex: auto; background-color: white;"
+                })
+            }
+        }
+})
+
+
