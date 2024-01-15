e1d450096707c22456e75b6700a6bee6a85cb374
TheBreakfastBun <138614756+TheBreakfastBun@users.noreply.github.com>
2024-01-15 17:06:08 +0200

    Intial files and 16X16 grid layout

---
+++ script.js
@@ -0,0 +1,15 @@
+const body = document.querySelector('body');
+const container = document.createElement("div");
+container.style = "display: flex; height: 100vh; width: 100%;"
+body.appendChild(container);
+
+for (let x = 0; x < 16; ++x){
+    const div = document.createElement('div');
+    div.style = "border: 1px solid white; flex: auto; display: flex; flex-direction: column;";
+    container.appendChild(div);
+    for (let y = 0; y < 16; ++y){
+        const yDiv = document.createElement('div');
+        yDiv.style = "border: 1px solid white; flex: auto;";
+        div.appendChild(yDiv);
+    }
+}
